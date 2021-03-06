c9b385083f476da08eaa0f017ba11cce7df7eed2
John Comeau <jc@unternet.net>
2021-03-05 20:17:49 -0800

    no speed increase but cleaner code

--- rfc7914.py
+++ rfc7914.py
@@ -5,7 +5,7 @@ minimalist implementation of rfc7914 optimized for litecoin-style scrypt hash
 N=1024, r=1, p=1, dkLen=32
 '''
 # pylint: disable=invalid-name, too-many-arguments
-import sys, os, logging, ctypes, struct  # pylint: disable=multiple-imports
+import sys, os, logging, ctypes  # pylint: disable=multiple-imports
 from hashlib import pbkdf2_hmac
 from collections import OrderedDict  # pylint: disable=unused-import
 
@@ -379,12 +379,13 @@ def xor(*arrays):
     >>> xor(b'\x55\x55\x55\x55', b'\xaa\xaa\xaa\xaa')
     bytearray(b'\xff\xff\xff\xff')
     '''
-    assert len(set(map(len, arrays))) == 1  # must all be the same length
+    assert len(arrays) == 2  # let's limit it to two for our needs
+    lengths = set(map(len, arrays))
+    assert len(lengths) == 1  # must all be the same length
     result = bytearray(arrays[0])
-    for i in range(1, len(arrays)):
-        #logging.debug('xor %r with %r', truncate(result), truncate(arrays[i]))
-        for j in range(len(result)):
-            result[j] ^= arrays[i][j]
+    #logging.debug('xor %r with %r', truncate(result), truncate(arrays[1]))
+    for i in range(1, len(result)):
+        result[i] ^= arrays[1][i]
     #logging.debug('xor result: %r', truncate(bytes(result)))
     return result
 
