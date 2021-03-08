3f876177fea76c2ee6a36b2c623e5cacaf3ee83c
John Comeau <jc@unternet.net>
2021-03-08 05:28:51 +0000

    finally debugging output is what I want

--- rfc7914.cpp
+++ rfc7914.cpp
@@ -7,13 +7,6 @@ using namespace std;
 #define R(a,b) (((a) << (b)) | ((a) >> (32 - (b))))
 typedef uint32_t uint32;  // for code copied from spec
 extern "C" {  // prevents name mangling
-    const char *digits = "0123456789abcdef";
-
-    char *hexnybbles(char n)  // because I can't figure out streams
-    {
-        char out[3] = {digits[n >> 4], digits[n & 0xff], 0};
-        return out;
-    }
 
     void showbytes(char *bytes, int length=64)  // for debugging
     // https://stackoverflow.com/a/10600155
@@ -22,7 +15,7 @@ extern "C" {  // prevents name mangling
         cerr << hex << setw(8) << setfill('0') << &bytes << ": ";
         for (int i = 0; i < length; i++)
         {
-            cerr << hexnybbles(bytes[i]);
+            cerr << setfill('0') << setw(2) << hex << (bytes[i] & 0xff);
         }
         cerr << endl;
     }
