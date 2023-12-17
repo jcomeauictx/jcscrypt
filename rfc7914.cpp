90fbffd12505a8d4c9a6a7b9bb74f20f3c8aa696
John Comeau <jcomeau@unternet.net>
2023-12-18 00:56:52 +0100

    added call signatures for two newer implementations

--- rfc7914.cpp
+++ rfc7914.cpp
@@ -58,6 +58,8 @@ extern "C" {  // prevents name mangling
 
     void salsa20_unrolled(uint32_t out[16], uint32_t in[16]);
     void salsa20_unaligned(uint32_t out[16], uint32_t in[16]);
+    void salsa20_aligned64(uint32_t out[16], uint32_t in[16]);
+    void salsa20_macro64(uint32_t out[16], uint32_t in[16]);
     void salsa20(uint32_t out[16], uint32_t in[16]);
 
     void showbytes(const void *addr, const void *bytes,
