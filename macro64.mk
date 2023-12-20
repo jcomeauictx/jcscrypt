57acf983e035b825f7742f30ae2e003ecafd0503
John Comeau <jcomeau@unternet.net>
2023-12-20 04:26:20 +0100

    fixed to remove .o *before* as well

--- macro64.mk
+++ macro64.mk
@@ -1,4 +1,5 @@
 all: salsa20_macro64
 %: %.S
+	rm -f $*.o  # so the object without `main` won't go into executable
 	CPPFLAGS=-DBUILD_EXECUTABLE $(MAKE) -B $@
 	rm -f $*.o  # so the object with `main` won't go into libsalsa.a
